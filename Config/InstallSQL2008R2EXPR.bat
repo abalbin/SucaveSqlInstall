9a84e5c85b7c1a88e144515c9ec61fcd4b8235b2
Arturo Balbin SBS <r2d2klapa@gmail.com>
2014-06-05 14:11:48 -0500

    included all configs in bat

--- Config/InstallSQL2008R2EXPR.bat
+++ Config/InstallSQL2008R2EXPR.bat
@@ -5,3 +5,10 @@ time/t
 "C:\SQL\install_files\setup.exe" /SAPWD="<password>" /ConfigurationFile="C:\SQL\ConfigurationFile.ini"
 date/t
 time/t
+echo Se instalo SQL Server 2008 R2 Express correctamente :)
+echo Abriendo puertos del firewall!!!
+netsh firewall set portopening protocol = TCP port = 1433 name = SQLPort mode = ENABLE scope = SUBNET profile = CURRENT
+echo se abrieron los puerto correctamente :))
+echo ahora solo falta configurar el puerto TCP del SQL! =/
+cscript "C:\SQL\tcpConfig.vbs"
+echo listo... ahora es cuestion de probar!
